999306ecba174c9292abd026202a775dca0226d9
Fei Guo <fguo@cienet.com>
2024-10-10 11:40:20 +0800

    finetune llama3.2 11b with one node

--- llama2-finetuning-slurm/files/fine-tune-slurm.sh
+++ llama2-finetuning-slurm/files/fine-tune-slurm.sh
@@ -9,4 +9,4 @@ export CONDA_BASE=/opt/conda
 source $CONDA_BASE/bin/activate base
 conda activate llama2
 cd $SLURM_SUBMIT_DIR
-python /data_bucket/fine-tune.py
+torchrun --nnodes 1 --nproc_per_node 8  fine-tune.py --enable_fsdp --lr 1e-5  --num_epochs 3 --batch_size_training 2 --model_name unsloth/Llama-3.2-11B-Vision-Instruct --dist_checkpoint_root_folder ./finetuned_model --dist_checkpoint_folder fine-tuned  --use_fast_kernels --dataset "custom_dataset" --custom_dataset.test_split "test" --custom_dataset.file "./ocrvqa_dataset.py"  --run_validation True --batching_strategy padding
